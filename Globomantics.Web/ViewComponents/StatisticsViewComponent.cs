6dcea5d3c7d23289e3dec9695cd7cbb7dfc07e07
Alexander Klassanov <klassanov@gmail.com>
2021-02-15 23:59:43 +0200

    Added ViewComponent to the Layout page

---
+++ Globomantics.Web/ViewComponents/StatisticsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Globomantics.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globomantics.Web.ViewComponents
+{
+    public class StatisticsViewComponent : ViewComponent
+    {
+        private readonly IConferenceService conferenceService;
+
+        public StatisticsViewComponent(IConferenceService conferenceService)
+        {
+            this.conferenceService = conferenceService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await conferenceService.GetStatistics());
+        }
+    }
+}
