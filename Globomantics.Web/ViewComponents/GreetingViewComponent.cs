1e71ccd47d4f8395fbc8dd68d49d32a3e18bd2ef
Alexander Klassanov <klassanov@gmail.com>
2021-02-16 10:14:25 +0200

    Greeting view component added

---
+++ Globomantics.Web/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globomantics.Web.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string greeting)
+        {
+            return View((object)greeting);
+        }
+    }
+}
